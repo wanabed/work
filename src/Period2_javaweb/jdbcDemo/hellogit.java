8467da1d0b41f5c163182fa477e4ac2dcb66445a
Lebron <759472253@qq.com>
2022-09-02 09:35:10 +0800

    master first commit

---
+++ src/Period2_javaweb/jdbcDemo/hellogit.java
@@ -0,0 +1,16 @@
+package Period2_javaweb.jdbcDemo;
+
+/**
+ * @Auther: zhoucan
+ * @Date: 2022/9/2-09-02-9:33
+ * @Description：Period2_javaweb.jdbcDemo
+ */
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+        System.out.println("hello world5");
+    }
+}
