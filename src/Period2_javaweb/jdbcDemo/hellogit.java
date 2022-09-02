88ac8120d06a7c57e9d70d89e5584fc6ae3dca24
Lebron <759472253@qq.com>
2022-09-02 11:28:09 +0800

    随便玩玩

--- src/Period2_javaweb/jdbcDemo/hellogit.java
+++ src/Period2_javaweb/jdbcDemo/hellogit.java
@@ -7,10 +7,6 @@ package Period2_javaweb.jdbcDemo;
  */
 public class hellogit {
     public static void main(String[] args) {
-        System.out.println("hello world1");
-        System.out.println("hello world2");
-        System.out.println("hello world3");
-        System.out.println("hello world4");
-        System.out.println("change 123");
+        System.out.println("随便改改");
     }
 }
